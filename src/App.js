a3f94f3db576a11d9044021e996bc8ea0f2582c4
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-08-10 16:30:26 -0400

    added component files and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import {React} from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Main from "./Main";
+import About from "./About"
+import Fave from "./Fave"
 function App() {
   return (
-    <h1>hi</h1>
+    <>
+    <h1>app</h1>
+    <Routes>
+      <Route index path="/" element={<Main/>}></Route>
+      <Route path="about" element={<About/>}></Route>
+      <Route path="fave" element={<Fave/>}></Route>
+    </Routes>
+    </>
    );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import conditions from './Conditions'
+import Inputs from './Inputs'
+
+export default function Main() {
+  return (
+    <div>Main</div>
+  )
+}
